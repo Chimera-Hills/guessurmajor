73bc921b7f2e9d50a4946fb987e10b38112e8283
Chimera-Hills <kimischimera@gmail.com>
2023-11-22 15:01:13 -0800

    Simple Set-up
    Create HTML, CSS, and JS files.
    Set up default look/template for main page.

---
+++ js/master.js
@@ -0,0 +1,30 @@
+/*
+master.js - 
+Author(s): Kevin Hong, add name here
+Date: 22 November, 2023
+*/
+
+// Button CSS
+
+// Change the button's appearance when the mouse enters it
+$("#play").mouseenter(function () {
+    $(this).addClass("buttonHover");
+});
+
+// Reverts it back once mouse exits
+$("#play").mouseleave(function () {
+    $(this).removeClass("buttonHover");
+});
+
+// Change the button's appearance when the mouse button is clicked downed on it
+$("#play").mousedown(function () {
+    $(this).addClass("buttonClick");
+
+    // Bind mouseup to the document to handle cases where the mouse is released outside the button
+    // From ChatGPT
+    $(document).mouseup(function () {
+        $("#play").removeClass("buttonClick");
+        // Unbind the mouseup event after it's been triggered once
+        $(document).off("mouseup");
+    });
+});
