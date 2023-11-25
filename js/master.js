4c8f3de5928dbb5c64372c829577189d1923c8aa
Chimera-Hills <kimischimera@gmail.com>
2023-11-24 23:16:35 -0800

    Few minor changes to CSS and buttons
    Made buttons interact with hover and click through CSS.

--- js/master.js
+++ js/master.js
@@ -7,23 +7,48 @@ Date: 22 November, 2023
 // Button CSS
 
 // Change the button's appearance when the mouse enters it
-$("#play").mouseenter(function () {
+$("button").mouseenter(function () {
     $(this).addClass("buttonHover");
 });
 
 // Reverts it back once mouse exits
-$("#play").mouseleave(function () {
+$("button").mouseleave(function () {
     $(this).removeClass("buttonHover");
 });
 
 // Change the button's appearance when the mouse button is clicked downed on it
-$("#play").mousedown(function () {
+$("button").mousedown(function () {
     $(this).addClass("buttonClick");
 
     // Bind mouseup to the document to handle cases where the mouse is released outside the button
     // From ChatGPT
     $(document).mouseup(function () {
-        $("#play").removeClass("buttonClick");
+        $("button").removeClass("buttonClick");
+        // Unbind the mouseup event after it's been triggered once
+        $(document).off("mouseup");
+    });
+});
+
+// Menu CSS
+
+// Change the icon's appearance when the mouse enters it
+$(".icon").mouseenter(function () {
+    $(this).addClass("iconHover");
+});
+
+// Reverts it back once mouse exits
+$(".icon").mouseleave(function () {
+    $(this).removeClass("iconHover");
+});
+
+// Change the icon's appearance when the mouse button is clicked downed on it
+$(".icon").mousedown(function () {
+    $(this).addClass("iconClick");
+
+    // Bind mouseup to the document to handle cases where the mouse is released outside the button
+    // From ChatGPT
+    $(document).mouseup(function () {
+        $(".icon").removeClass("iconClick");
         // Unbind the mouseup event after it's been triggered once
         $(document).off("mouseup");
     });
